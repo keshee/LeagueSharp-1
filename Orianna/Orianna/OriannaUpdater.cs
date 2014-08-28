302fae44fd141e7082e051f91b26265078ca0e88
Trelli <trellidev@gmail.com>
2014-08-28 16:21:35 -0500

    Update Orianna for latest common lib

--- Orianna/Orianna/OriannaUpdater.cs
+++ Orianna/Orianna/OriannaUpdater.cs
@@ -10,7 +10,7 @@ namespace Orianna
 {
     class OriannaUpdater
     {
-        private const int localversion = 9;
+        private const int localversion = 10;
         internal static bool isInitialized;
 
         internal static void InitializeOrianna()
